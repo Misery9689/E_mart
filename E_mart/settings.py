4891d0e72e6b8d131adaee85e38d82c1479e91b3
Trakh9689 <trakh2500@gmail.com>
2025-08-01 14:49:24 +0700

    Allow Host

--- E_mart/settings.py
+++ E_mart/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e-mart-v2kw.onrender.com', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # E_mart/settings.py
 INSTALLED_APPS = [
