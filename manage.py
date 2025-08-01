3c7e3246cedefade94797ff0e55a20129de851a3
Trakh9689 <trakh2500@gmail.com>
2025-08-01 14:16:56 +0700

    Config Manage

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -21,7 +21,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-=======
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
@@ -44,4 +44,4 @@ def main():
 
 if __name__ == '__main__':
     main()
->>>>>>> 40351b8156c73bab5e97d8849973739a2a33751e
+
